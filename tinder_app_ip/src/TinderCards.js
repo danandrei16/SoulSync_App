246dbeb470bccc5c4fc7370a6423f881f64ba8cc
Dan Ionescu <dananndrei16@gmail.com>
2024-05-28 20:00:29 +0300

    ne b*l*m

--- tinder_app_ip/src/TinderCards.js
+++ tinder_app_ip/src/TinderCards.js
@@ -11,7 +11,38 @@ function TinderCards() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [showDetails, setShowDetails] = useState(false); // State to control showing user details
+    const [showMatch, setShowMatch] = useState(false); // State to control showing match confirmation
     const { currentUser } = useAuth(); // Get the currently authenticated user from the auth context
+
+    // COMPATIBILITY FUNCTION
+    const calculateCompatibilityScore = (user1, user2) => {
+        let score = 0;
+
+        const zodiacCompatibility = {
+            Aries: { Aries: 60, Taurus: 65, Gemini: 65, Cancer: 65, Leo: 90, Virgo: 45, Libra: 70, Scorpio: 80, Sagittarius: 10, Capricorn: 50, Aquarius: 55, Pisces: 65 },
+            Taurus: { Aries: 60, Taurus: 70, Gemini: 70, Cancer: 80, Leo: 70, Virgo: 90, Libra: 75, Scorpio: 85, Sagittarius: 50, Capricorn: 95, Aquarius: 80, Pisces: 85 },
+            Gemini: { Aries: 70, Taurus: 70, Gemini: 75, Cancer: 60, Leo: 80, Virgo: 75, Libra: 90, Scorpio: 60, Sagittarius: 75, Capricorn: 50, Aquarius: 90, Pisces: 50 },
+            Cancer: { Aries: 65, Taurus: 80, Gemini: 60, Cancer: 75, Leo: 70, Virgo: 75, Libra: 60, Scorpio: 95, Sagittarius: 55, Capricorn: 45, Aquarius: 70, Pisces: 90 },
+            Leo: { Aries: 90, Taurus: 70, Gemini: 80, Cancer: 70, Leo: 85, Virgo: 75, Libra: 65, Scorpio: 75, Sagittarius: 95, Capricorn: 45, Aquarius: 70, Pisces: 75 },
+            Virgo: { Aries: 45, Taurus: 90, Gemini: 75, Cancer: 75, Leo: 75, Virgo: 70, Libra: 80, Scorpio: 85, Sagittarius: 70, Capricorn: 95, Aquarius: 50, Pisces: 70 },
+            Libra: { Aries: 70, Taurus: 75, Gemini: 90, Cancer: 60, Leo: 65, Virgo: 80, Libra: 80, Scorpio: 85, Sagittarius: 80, Capricorn: 85, Aquarius: 95, Pisces: 50 },
+            Scorpio: { Aries: 80, Taurus: 85, Gemini: 60, Cancer: 95, Leo: 75, Virgo: 85, Libra: 85, Scorpio: 90, Sagittarius: 80, Capricorn: 65, Aquarius: 60, Pisces: 95 },
+            Sagittarius: { Aries: 90, Taurus: 50, Gemini: 75, Cancer: 55, Leo: 95, Virgo: 70, Libra: 80, Scorpio: 85, Sagittarius: 85, Capricorn: 55, Aquarius: 60, Pisces: 75 },
+            Capricorn: { Aries: 50, Taurus: 95, Gemini: 50, Cancer: 45, Leo: 45, Virgo: 95, Libra: 85, Scorpio: 65, Sagittarius: 55, Capricorn: 85, Aquarius: 70, Pisces: 85 },
+            Aquarius: { Aries: 55, Taurus: 80, Gemini: 90, Cancer: 70, Leo: 70, Virgo: 50, Libra: 95, Scorpio: 60, Sagittarius: 60, Capricorn: 70, Aquarius: 80, Pisces: 55 },
+            Pisces: { Aries: 65, Taurus: 85, Gemini: 50, Cancer: 90, Leo: 75, Virgo: 70, Libra: 50, Scorpio: 95, Sagittarius: 75, Capricorn: 85, Aquarius: 55, Pisces: 80 }
+            };
+        // Example criteria for compatibility score calculation
+        if (user1.gender === user2.preference && user2.gender === user1.preference) {
+            score += 50;
+        }
+        
+        score += zodiacCompatibility[user1.starSign][user2.starSign];
+        
+        // Add more criteria as needed
+
+        return score;
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,7 +55,14 @@ function TinderCards() {
                 const querySnapshot = await firebase.firestore().collection('people').where(firebase.firestore.FieldPath.documentId(), '!=', currentUser.uid).get();
                 const fetchedPeople = querySnapshot.docs
                     .map(doc => ({ id: doc.id, ...doc.data() }))
-                    .filter(person => !currentUserData.swipes || !currentUserData.swipes.includes(person.id));
+                    .filter(person => !currentUserData.swipes || !currentUserData.swipes.includes(person.id))
+                    .map(person => ({
+                        ...person,
+                        compatibilityScore: calculateCompatibilityScore(currentUserData, person) // Calculate compatibility score
+                    }));
+
+                // Sort people by compatibility score in descending order
+                fetchedPeople.sort((a, b) => b.compatibilityScore - a.compatibilityScore);
         
                 setPeople(fetchedPeople);
                 setLoading(false);
@@ -36,9 +74,6 @@ function TinderCards() {
             }
         };
         
-        fetchData();
-        
-
         if (currentUser) {
             fetchData();
         }
@@ -59,6 +94,22 @@ function TinderCards() {
                     updatedSwipes = [personId];
                 }
                 await currentUserRef.set({ swipes: updatedSwipes }, { merge: true });
+
+                // Check if the swiped person has also swiped right on the current user
+                const swipedPersonRef = firebase.firestore().collection('people').doc(personId);
+                const swipedPersonDoc = await swipedPersonRef.get();
+                const swipedPersonData = swipedPersonDoc.data();
+                if (swipedPersonData && swipedPersonData.swipes && swipedPersonData.swipes.includes(currentUser.uid)) {
+                    setShowMatch(true);
+                    
+                    // Save notification to Firestore
+                    const notificationMessage = `You matched with ${swipedPersonData.name}!`;
+                    await currentUserRef.update({ notifications: firebase.firestore.FieldValue.arrayUnion(notificationMessage) });
+
+                    setTimeout(() => {
+                        setShowMatch(false);
+                    }, 3000); // Hide match confirmation after 3 seconds
+                }
             }
             
             if (currentIndex + 1 < people.length) {
@@ -68,7 +119,6 @@ function TinderCards() {
         } catch (error) {
             console.error('Error updating swipes:', error);
         }
-        
     };
 
     const toggleDetails = () => {
@@ -89,6 +139,11 @@ function TinderCards() {
 
     return (
         <div className='tinderCards__cardContainer'>
+            {showMatch && (
+                <div className="matchConfirmation">
+                    <p>You have matched!</p>
+                </div>
+            )}
             {/* Render the current and next cards */}
             {people.slice(currentIndex, currentIndex + 2).map((person, index) => (
                 <div key={index} style={{ position: 'absolute', width: '100%', left: 0, zIndex: index === 0 ? 2 : 1 }}>
@@ -99,7 +154,8 @@ function TinderCards() {
                         preventSwipe={['up', 'down']}
                     >
                         <div className='cardContainer'>
-                            <div style={{ backgroundImage: `url(${person.picture})` }} className='card'>
+                            <div style={{
+                                backgroundImage: `url(${person.picture})` }} className='card'>
                                 <h3>{person.name}, {person.age}</h3>
                                 {showDetails && (
                                     <div className="details">
